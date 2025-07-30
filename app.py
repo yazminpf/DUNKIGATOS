b365e18d59220c3a2b4ef65a52677ad90d1b2267
yazmin.p <luyaz1995@gmail.com>
2025-07-29 19:14:38 -0500

    Activa creación de tablas con db.create_all()

--- app.py
+++ app.py
@@ -219,7 +219,7 @@ def facturar():
 if __name__ == "__main__":
     with app.app_context():
         probar_conexion()
-        # db.create_all()
+        db.create_all()
 
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
